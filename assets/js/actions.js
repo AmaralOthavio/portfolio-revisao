0a63e5ffa5983f324eccf264e04f4dd20ce1ec9c
Othavio <167528849+AmaralOthavio@users.noreply.github.com>
2025-02-03 11:24:46 -0300

    Upload inicial dos arquivos

---
+++ assets/js/actions.js
@@ -0,0 +1,33 @@
+var extraBtn = document.getElementById('btn-extra')
+extraBtn.addEventListener('click', function() {
+    let extra1 = document.getElementById('extra1')
+    let extra2 = document.getElementById('extra2')
+    let extra3 = document.getElementById('extra3')
+
+    let display1 = getComputedStyle(extra1).display
+    if (display1 == 'none') {
+        extra1.style.display = 'block'
+        extra2.style.display = 'block'
+        extra3.style.display = 'block'
+        extraBtn.innerText = 'Mostrar menos'
+    }
+    else {
+        extra1.style.display = 'none'
+        extra2.style.display = 'none'
+        extra3.style.display = 'none'
+        extraBtn.innerText = 'Mostrar mais'
+    }
+})
+
+var planejaLogo = document.getElementById('planeja-facil')
+var rotacionado = false
+planejaLogo.addEventListener('click', function() {
+    if (rotacionado == false) {
+        planejaLogo.style.transform = 'rotate(360deg)'
+        rotacionado = true
+    }
+    else {
+        planejaLogo.style.transform = 'rotate(-360deg)'
+        rotacionado = false
+    }
+})
